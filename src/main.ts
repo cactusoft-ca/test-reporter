a6d89b76f8745efada1a146781dcdaaf29490a88
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2022-09-09 13:47:05 -0400

    Added check run url output

--- src/main.ts
+++ src/main.ts
@@ -193,6 +193,7 @@ class TestReporter {
     core.info(`Check run create response: ${resp.status}`)
     core.info(`Check run URL: ${resp.data.url}`)
     core.info(`Check run HTML: ${resp.data.html_url}`)
+    core.setOutput('url', resp.data.html_url)
 
     return results
   }
